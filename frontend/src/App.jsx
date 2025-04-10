a2614c5cf00582bd39b47b41b920ca1e3e8582e9
Cedzi-95 <cedricmasinzo@yahoo.com>
2025-04-10 15:22:39 +0200

    add front end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,73 @@
+import { useState, useRef, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  
+const [chatInput, setChatInput] = useState("");
+const [userName, setUserName] = useState("");
+const [messages, setMessages] = useState([]);
+const lastMessageId = useRef(0);
+
+useEffect(() => {
+  lastMessageId.current = messages.length;
+}, [messages]);
+
+
+useEffect(() => {
+  const timer = setInterval(() => {
+    console.log(lastMessageId.current);
+    fetch('http://localhost:5076/chat/' + lastMessageId.current)
+    .then(res => res.json())
+    .then(res => {
+      setMessages(old => [...old, ...res]);
+
+    });
+  }, 1000);
+
+  return () => {
+    clearInterval(timer);
+  };
+}, []);
+
+const sendMessage = () => {
+  fetch('http://localhost:5076/chat/', {
+    method: "POST",
+    headers: {
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify({
+      content: chatInput,
+      userName: userName,
+    })  
+  }).then(() => {
+    setChatInput("");
+  });
+};
+
+  return (
+    <>
+      <div>
+        {messages.map(message => {
+          return <div>
+            <span>{message.sentDateTime.toLocaleString()}</span>
+            <p>{message.content}</p>
+            <span>{message.userName}</span>
+          </div>
+        })}
+      </div>
+        
+        <div>
+          <label>Username</label>
+          <input value={userName} onChange={event => setUserName(event.target.value)} />
+        </div>
+
+        <div>
+          <label>Message</label>
+          <input value={chatInput} onChange={event => setChatInput(event.target.value)} />
+        </div>
+        <button onClick={sendMessage}>Send</button>
+    </>
+  )
+}
+
+export default App
